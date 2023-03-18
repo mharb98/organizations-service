3e088f51436889852e013ee77d81c18793466e8a
mharb98 <marwanharb65@outlook.com>
2023-03-18 17:49:14 +0200

    renamed moderators module to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BrokersModule } from '../brokers/brokers.module';
-import { ModeratorsConsumer } from './moderators.consumer';
+import { ModeratorsConsumer } from './users.consumer';
 import { ModeratorsService } from './moderators.service';
 import { ModeratorsRepository } from './repositories/moderators.repository';
 import { Moderator, ModeratorSchema } from './schema/moderator.schema';
